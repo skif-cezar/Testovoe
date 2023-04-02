e87331530a8120d17bad6e6bc34fcfe941669dc3
skif-cezar <skif.cezar@gmail.com>
2023-04-02 14:22:29 +0300

    feat: add not found ads component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import GlobalStyles from "src/app/global";
-import {AdsPage} from "./logic/pages/ads-page/AdsPage";
-import {AdProvider} from "./logic/store/Store";
+import {AdsPage} from "src/app/logic/pages/ads-page/AdsPage";
+import {AdProvider} from "src/app/logic/store/Store";
 
 /**
  * The main component in app
